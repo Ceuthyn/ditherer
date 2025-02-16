06edd086d58c96f16c5ab9283ae5e9c80b59b24b
cygnus <whenthefoundationcrumbles@gmail.com>
2025-02-16 04:11:26 +0200

    shouldve probably commited earlier

--- dithering nightmare/Form1.Designer.cs
+++ dithering nightmare/Form1.Designer.cs
@@ -28,13 +28,239 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.brightness = new System.Windows.Forms.TrackBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.contrast = new System.Windows.Forms.TrackBar();
+            this.imgsel = new System.Windows.Forms.Button();
+            this.saveimg = new System.Windows.Forms.Button();
+            this.outpbox = new System.Windows.Forms.PictureBox();
+            this.inpbox = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.datv = new System.Windows.Forms.TableLayoutPanel();
+            this.remc = new System.Windows.Forms.Button();
+            this.addc = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.brightness)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.contrast)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outpbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inpbox)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // brightness
+            // 
+            this.brightness.AutoSize = false;
+            this.brightness.Location = new System.Drawing.Point(6, 19);
+            this.brightness.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.brightness.Maximum = 5;
+            this.brightness.Minimum = -5;
+            this.brightness.Name = "brightness";
+            this.brightness.Size = new System.Drawing.Size(104, 45);
+            this.brightness.TabIndex = 0;
+            this.brightness.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.brightness.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.brightness);
+            this.groupBox1.Location = new System.Drawing.Point(12, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(116, 80);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "brightness";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.contrast);
+            this.groupBox2.Location = new System.Drawing.Point(128, 14);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(116, 80);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "contrast";
+            // 
+            // contrast
+            // 
+            this.contrast.AutoSize = false;
+            this.contrast.Location = new System.Drawing.Point(6, 19);
+            this.contrast.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.contrast.Maximum = 5;
+            this.contrast.Minimum = -5;
+            this.contrast.Name = "contrast";
+            this.contrast.Size = new System.Drawing.Size(104, 45);
+            this.contrast.TabIndex = 0;
+            this.contrast.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // imgsel
+            // 
+            this.imgsel.Location = new System.Drawing.Point(250, 14);
+            this.imgsel.Name = "imgsel";
+            this.imgsel.Size = new System.Drawing.Size(47, 80);
+            this.imgsel.TabIndex = 4;
+            this.imgsel.Text = "select image";
+            this.imgsel.UseVisualStyleBackColor = true;
+            this.imgsel.Click += new System.EventHandler(this.imgsel_Click);
+            // 
+            // saveimg
+            // 
+            this.saveimg.Location = new System.Drawing.Point(303, 14);
+            this.saveimg.Name = "saveimg";
+            this.saveimg.Size = new System.Drawing.Size(47, 80);
+            this.saveimg.TabIndex = 5;
+            this.saveimg.Text = "save image";
+            this.saveimg.UseVisualStyleBackColor = true;
+            this.saveimg.Click += new System.EventHandler(this.saveimg_Click);
+            // 
+            // outpbox
+            // 
+            this.outpbox.Location = new System.Drawing.Point(6, 19);
+            this.outpbox.Name = "outpbox";
+            this.outpbox.Size = new System.Drawing.Size(254, 246);
+            this.outpbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.outpbox.TabIndex = 8;
+            this.outpbox.TabStop = false;
+            // 
+            // inpbox
+            // 
+            this.inpbox.Location = new System.Drawing.Point(6, 19);
+            this.inpbox.Name = "inpbox";
+            this.inpbox.Size = new System.Drawing.Size(254, 246);
+            this.inpbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.inpbox.TabIndex = 9;
+            this.inpbox.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.AutoSize = true;
+            this.groupBox3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox3.Controls.Add(this.datv);
+            this.groupBox3.Controls.Add(this.remc);
+            this.groupBox3.Controls.Add(this.addc);
+            this.groupBox3.Controls.Add(this.groupBox1);
+            this.groupBox3.Controls.Add(this.groupBox2);
+            this.groupBox3.Controls.Add(this.imgsel);
+            this.groupBox3.Controls.Add(this.saveimg);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.groupBox3.Size = new System.Drawing.Size(542, 110);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            // 
+            // datv
+            // 
+            this.datv.CausesValidation = false;
+            this.datv.ColumnCount = 2;
+            this.datv.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.datv.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.datv.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.AddColumns;
+            this.datv.Location = new System.Drawing.Point(356, 14);
+            this.datv.Name = "datv";
+            this.datv.RowCount = 2;
+            this.datv.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.datv.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.datv.Size = new System.Drawing.Size(121, 80);
+            this.datv.TabIndex = 10;
+            // 
+            // remc
+            // 
+            this.remc.Location = new System.Drawing.Point(483, 56);
+            this.remc.Name = "remc";
+            this.remc.Size = new System.Drawing.Size(53, 38);
+            this.remc.TabIndex = 9;
+            this.remc.Text = "remove color";
+            this.remc.UseVisualStyleBackColor = true;
+            this.remc.Click += new System.EventHandler(this.remc_Click);
+            // 
+            // addc
+            // 
+            this.addc.Location = new System.Drawing.Point(483, 14);
+            this.addc.Name = "addc";
+            this.addc.Size = new System.Drawing.Size(53, 38);
+            this.addc.TabIndex = 8;
+            this.addc.Text = "add color";
+            this.addc.UseVisualStyleBackColor = true;
+            this.addc.Click += new System.EventHandler(this.addc_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.AutoSize = true;
+            this.groupBox4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox4.Controls.Add(this.inpbox);
+            this.groupBox4.Location = new System.Drawing.Point(12, 125);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(266, 284);
+            this.groupBox4.TabIndex = 11;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "input";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.AutoSize = true;
+            this.groupBox5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox5.Controls.Add(this.outpbox);
+            this.groupBox5.Location = new System.Drawing.Point(288, 125);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(266, 284);
+            this.groupBox5.TabIndex = 12;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(729, 450);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "ditherer";
+            ((System.ComponentModel.ISupportInitialize)(this.brightness)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.contrast)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outpbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inpbox)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar brightness;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TrackBar contrast;
+        private System.Windows.Forms.Button imgsel;
+        private System.Windows.Forms.Button saveimg;
+        private System.Windows.Forms.PictureBox outpbox;
+        private System.Windows.Forms.PictureBox inpbox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button remc;
+        private System.Windows.Forms.Button addc;
+        private System.Windows.Forms.TableLayoutPanel datv;
     }
 }
 
--- dithering nightmare/Form1.cs
+++ dithering nightmare/Form1.cs
@@ -3,18 +3,111 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace dithering_nightmare
-{
-    public partial class Form1: Form
-    {
-        public Form1()
-        {
+namespace dithering_nightmare{
+    public partial class Form1: Form{
+        List<Color> palette = new List<Color>{Color.FromArgb(0,0,0)};
+        Bitmap img;
+        Bitmap outp;
+        int width;
+        int height;
+        public Form1(){
             InitializeComponent();
+            upddat();
         }
+
+        private void trackBar1_Scroll(object sender, EventArgs e){
+
+        }
+
+        private void imgsel_Click(object sender, EventArgs e){
+            try{
+                var filsel = new OpenFileDialog();
+                filsel.Filter = "png files (*.png)|*.png";
+                if (filsel.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                this.img = new Bitmap(filsel.FileName);
+            } catch (Exception){
+                MessageBox.Show("uh oh", "app broke call a doctor", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; //idk man just fail
+            }
+            inpbox.Image = this.img;
+        }
+
+        private void upddat() {
+            List<Panel> p = new List<Panel>();
+            datv.Controls.Clear();
+            for (int i = 0; i < this.palette.Count; i++) {
+                p.Add(new Panel());
+                p[i].BackColor = this.palette[i];
+                p[i].Dock = DockStyle.Fill;
+                p[i].Name = "panel" + i;
+                datv.Controls.Add(p[i]);
+            }
+            datv.ColumnCount = (int) Math.Ceiling((double) this.palette.Count / 2);
+            datv.RowCount = 2;
+            datv.RowStyles.Clear();
+            datv.ColumnStyles.Clear();
+            for(int i = 0; i<2; i++){
+                datv.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50f));
+            }
+            for (int i = 0; i < datv.ColumnCount; i++){
+                System.Diagnostics.Debug.WriteLine(datv.ColumnCount);
+                datv.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100/datv.ColumnCount));
+            }
+        }
+        private void saveimg_Click(object sender, EventArgs e){
+            try{
+                if (this.outp == null) return;
+                var downl = new SaveFileDialog();
+                downl.Filter = "PNG files (*.png)|*.png";
+                downl.Title = "save image";
+                downl.RestoreDirectory = true;
+                downl.OverwritePrompt = true;
+                if(downl.ShowDialog() == DialogResult.OK){
+                    outp.Save(downl.FileName, ImageFormat.Png); //no other formats because they suck
+                }
+            } catch (Exception) { } // bad but dont care rn
+            return;
+        }
+
+        private void addc_Click(object sender, EventArgs e){
+            var colord = new ColorDialog();
+            if(colord.ShowDialog() == DialogResult.OK){
+                //System.congrDiagnostics.Debug.WriteLine(colord.Color);
+                this.palette.Add(colord.Color);
+                for(int i = 0; i<this.palette.Count; i++){
+                    System.Diagnostics.Debug.WriteLine(this.palette[i]);
+                }
+                upddat();
+                rend();
+            }
+        }
+
+        private void remc_Click(object sender, EventArgs e){
+            if(this.palette.Count != 0) this.palette.RemoveAt(this.palette.Count -1);
+            upddat();
+            rend();
+        }
+
+        private int clampedadd(int a, int b){
+            return Math.Max(Math.Min(a + b, 768), 0);
+        }
+
+
+
+        private void rend(){
+
+
+        }
+
     }
 }
